a0aceffff027b5edb056d653131cf2c5666d60ed
shahmeerdv <mshahmeer86@gmail.com>
2025-03-09 23:15:51 +0500

    commit three

---
+++ dynamic.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define N 500
+#define M 500
+#define P 500
+
+void initializeMatrix(int matrix[N][M]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+}
+
+void matrixMultiplyDynamic(int A[N][M], int B[M][P], int C[N][P]) {
+    #pragma omp parallel for collapse(2) schedule(dynamic)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < P; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < M; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int A[N][M], B[M][P], C[N][P];
+
+    srand(time(NULL));
+    initializeMatrix(A);
+    initializeMatrix(B);
+
+    double start = omp_get_wtime();
+    matrixMultiplyDynamic(A, B, C);
+    double end = omp_get_wtime();
+
+    printf("Dynamic Scheduling Execution Time: %f seconds\n", end - start);
+
+    return 0;
+}
